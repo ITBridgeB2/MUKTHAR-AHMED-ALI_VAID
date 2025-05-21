39d079627b55f60df7fbe670079cf6f80f15ff10
Mukthar <muktharahmedali789@gmail.com>
2025-05-21 14:01:31 +0530

    updated

--- client-side/src/dashBoard.js
+++ client-side/src/dashBoard.js
@@ -1,24 +1,7 @@
 import React from 'react';
 
 export default function Dashboard() {
-  const hospitals = [
-    {
-      hospital_name: "Jayadeva Institute of Cardiology",
-      contact_number: "9012345678"
-    },
-    {
-      hospital_name: "Narayana Institute of Cardiac Sciences",
-      contact_number: "9112233445"
-    },
-    {
-      hospital_name: "St. Martha’s Heart Centre",
-      contact_number: "9556677889"
-    },
-    {
-      hospital_name: "Manipal Hospital", // Assume this one has a cardiology wing
-      contact_number: "9888997766"
-    }
-  ];
+
 
   return (
     <div style={{ padding: '20px' }}>
